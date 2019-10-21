516f1d31b1b543e2f3f2fe6cb44a3847db06cc7a
TB831 <druu831@gmail.com>
2019-10-21 14:00:43 -0700

    File IO errors

--- script.py
+++ script.py
@@ -1,7 +1,9 @@
-with open('test.txt', mode='a') as myFile: # Standard way of opening a file. automatically closes file.
-  text = myFile.write('hey it\'s me!')
-  print(text)
-
-with open('sad.txt', mode='w') as myFile: # Creates a new file called sad.txt and writes a :(
-  sad = myFile.write(':(')
-  print(text)
+try:
+  with open('text.txt', mode='w') as myFile:
+    print(myFile.readlines())
+except FileNotFoundError as err:
+  print('file doesn\'t exist')
+  raise err
+except IOError as err:
+  print('IO Error')
+  raise err
